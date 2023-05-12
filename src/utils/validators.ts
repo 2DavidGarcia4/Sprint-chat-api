1c0c2a0ca808696f47d7460c04ccd335da978ee4
2DavidGarcia4 <gacub20ru@gmail.com>
2023-05-11 20:25:35 -0600

    add mising data validator

---
+++ src/utils/validators.ts
@@ -0,0 +1,16 @@
+import { Response } from "express";
+import { setErrorResposne } from './functions'
+
+function missingData(res: Response, fields: any[], fieldsMap?: { [key: string]: string }) {
+  if(!fields.every(f=> f)) {
+    setErrorResposne(res, 'Missing data', 400, fieldsMap)
+    return true
+  }
+
+  return false
+}
+
+
+export default {
+  missingData
+}
