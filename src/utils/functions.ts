0e0c9e735d62f5531dd5b86203379ccf79eeac65
2DavidGarcia4 <gacub20ru@gmail.com>
2023-05-11 20:25:54 -0600

    update

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -15,18 +15,6 @@ export function comparePassword(plainPassword: string, hashPass: string) {
   return bcrypt.compareSync(plainPassword, hashPass);
 };
 
-const uuid = () => {
-  let arr = []
-  arr[8] = '-', arr[13] = '-', arr[18] = '-', arr[23] = '-'
-  
-  for(let i=0; i<36; i++){
-    let el = arr[i], random = (Math.floor(Math.random()*16)+1)
-    if(!el) arr[i] = random.toString(17)
-  }
-
-  return arr.join('')
-}
-
 export function setErrorResposne(res: Response, message: string, status=400, fields?: { [key: string]: string }) {
   if(fields){
     res.status(status).json({
