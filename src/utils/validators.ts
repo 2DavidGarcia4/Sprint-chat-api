f9d8d851104981aca8a515f7e1debc1a11133689
2DavidGarcia4 <gacub20ru@gmail.com>
2023-05-12 20:11:03 -0600

    update mising data validator

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -2,7 +2,7 @@ import { Response } from "express";
 import { setErrorResposne } from './functions'
 
 function missingData(res: Response, fields: any[], fieldsMap?: { [key: string]: string }) {
-  if(!fields.every(f=> f)) {
+  if(!fields.every(f=> typeof f != 'undefined')) {
     setErrorResposne(res, 'Missing data', 400, fieldsMap)
     return true
   }
